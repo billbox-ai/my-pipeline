9339fff2e9935e052d87663836d808505b1636ba
Mike Whalen <mike@billbox.ai>
2023-05-31 18:02:22 +0000

    Updated to use stage

--- src/infrastructure/lib/my-pipeline-lambda-stack.ts
+++ src/infrastructure/lib/my-pipeline-lambda-stack.ts
@@ -5,36 +5,45 @@ import { Construct } from "constructs";
 import { StringParameter } from "aws-cdk-lib/aws-ssm";
 
 export class MyLambdaStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props?: StackProps,
+    stageName?: string
+  ) {
     super(scope, id, props);
 
     // get latest version of base layer
     const latestBaseLayerArn = StringParameter.valueForStringParameter(
       this,
-      "BaseLayerArn"
+      "/BaseLayerStack/BaseLayerArn-" + stageName
     );
 
     // TODO extract this to billbox lambda construct
-    const layerExampleLambda = new NodejsFunction(this, "LayerExampleLambda", {
-      entry: "./src/functions/parse-function/index.ts",
-      handler: "handler",
-      runtime: Runtime.NODEJS_16_X,
-      environment: {
-        REGION: "us-east-1",
-      },
-      timeout: Duration.seconds(15),
-      memorySize: 128,
-      bundling: {
-        sourceMap: true,
-        externalModules: ["aws-sdk", "billbox-base"],
-      },
-      layers: [
-        LayerVersion.fromLayerVersionArn(
-          this,
-          "BaseLayerVersionName",
-          latestBaseLayerArn
-        ),
-      ],
-    });
+    const layerExampleLambda = new NodejsFunction(
+      this,
+      "LayerExampleLambda-" + stageName,
+      {
+        entry: "./src/functions/parse-function/index.ts",
+        handler: "handler",
+        runtime: Runtime.NODEJS_16_X,
+        environment: {
+          REGION: "us-east-1",
+        },
+        timeout: Duration.seconds(15),
+        memorySize: 128,
+        bundling: {
+          sourceMap: true,
+          externalModules: ["aws-sdk", "billbox-base"],
+        },
+        layers: [
+          LayerVersion.fromLayerVersionArn(
+            this,
+            "BaseLayerVersionName",
+            latestBaseLayerArn
+          ),
+        ],
+      }
+    );
   }
 }
